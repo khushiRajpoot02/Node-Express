cb60993f594d00f1d8350016f793f165ab5f913a
khushiRajpoot02 <khushisinghbhu02@gmail.com>
2023-09-27 18:36:18 +0530

    inbuiltMdlWare

--- Node/app.js
+++ Node/app.js
@@ -1,30 +1,23 @@
-// Express middleware functions
+// Express middleware functions (Inbult)
+// npm i morgan
 const express = require('express')
-const authorize = require('./authMiddleWare')
-const logger = require('./middleWare')
 const app = express();
-// req => middleware = > res
-// const logger = (req, res, next)=>{
-//     const method = req.method;
-//     const url = req.url;
-//     const year = new Date().getFullYear();
-//     console.log(method, url, year);
-//     res.send('Testing');
-//     next();
-// }
-// instead of using logger inside every app.get method, we can use app.use() function to implement the middleware functionality
-app.use([logger,authorize])
-// or we can explicitly give path to the app.use(',/api') here after every ./api middleware will be applied 
-app.get('/' , (req, res)=>{
-   console.log('user hit the server') 
-   res.send('Home');                     
+const morgan = require('morgan');
+
+app.use(morgan('tiny')) // morgan('tiny')=>means for all route 
+app.get('/', (req, res)=>{
+    res.send('Home page')
 })
-app.get('/About' , (req, res)=>{
-    // console.log('user hit the server') 
-   
-    res.send('About');  
-    console.log(req.user);                   
- })
+
+app.get('/About', (req, res)=>{
+    res.send('About page');
+})
+
+app.get('/api/product', (req, res)=>{
+    res.send('Product');
+})
+
+
 app.listen(5000, ()=>{
     console.log("Server is listing on port 5000");
 })
