82674cd6a9beb0582498b0fa70db7068a5f49a07
khushiRajpoot02 <khushisinghbhu02@gmail.com>
2023-09-28 18:22:55 +0530

    get and psot using form

--- Node/app.js
+++ Node/app.js
@@ -1,23 +1,23 @@
-// Express middleware functions (Inbult)
-// npm i morgan
+// GET POST PUT DELETE methods understanding
 const express = require('express')
 const app = express();
-const morgan = require('morgan');
-
-app.use(morgan('tiny')) // morgan('tiny')=>means for all route 
-app.get('/', (req, res)=>{
-    res.send('Home page')
+let {people} = require('./data')
+// GET method
+app.use(express.static('./methods-public'))
+app.get('/api/people', (req, res)=>{
+    res.status(200).send({success : true , data : people})
 })
+// POST method using HTML form
+// using middleware for showing form entered data
+app.use(express.urlencoded({extended : false}));
 
-app.get('/About', (req, res)=>{
-    res.send('About page');
+app.post('/login', (req, res)=>{
+    const {name} = req.body
+    if(name){
+        res.status(200).send(`Welcome ${name}`);
+    }
+    res.status(401).send('Please use credntials');
 })
-
-app.get('/api/product', (req, res)=>{
-    res.send('Product');
-})
-
-
 app.listen(5000, ()=>{
     console.log("Server is listing on port 5000");
 })
