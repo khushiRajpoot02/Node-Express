c5a6b2b05b28c1b1ca4e3fcaeced3c31543eb84e
khushiRajpoot02 <khushisinghbhu02@gmail.com>
2023-09-28 20:53:32 +0530

    all methods using postman

--- Node/app.js
+++ Node/app.js
@@ -1,4 +1,4 @@
-// GET POST PUT DELETE methods understanding
+// GET POST PUT DELETE methods understanding using POSTMAN
 const express = require('express')
 const app = express();
 let {people} = require('./data')
@@ -7,10 +7,10 @@ app.use(express.static('./methods-public'))
 app.get('/api/people', (req, res)=>{
     res.status(200).send({success : true , data : people})
 })
-// POST method using HTML form
-// using middleware for showing form entered data
-app.use(express.urlencoded({extended : false}));
 
+app.use(express.urlencoded({extended : false}));
+// below middleware is used for sending json data 
+app.use(express.json());
 app.post('/login', (req, res)=>{
     const {name} = req.body
     if(name){
@@ -18,6 +18,51 @@ app.post('/login', (req, res)=>{
     }
     res.status(401).send('Please use credntials');
 })
+app.post('/api/people', (req, res)=>{
+    const {name} = req.body;
+    if(!name){
+       return res.status(404).json({success : false , message : "please provide name"});
+    }
+ res.status(200).send({success : true, person : name})
+})
+
+app.post('/api/people/postman', (req, res)=>{
+    const {name} = req.body
+    if(!name){
+       return res.status(400).json('please add name');
+    }
+    res.status(200).json([...people, {name}]);
+})
+// put method (updating the data)
+  app.put('/api/people/postman/:id',(req, res)=>{
+       const {id} = req.params;
+       const {name} = req.body;
+       const person = people.find((person)=>person.id === Number(id))
+       if(!person){
+        return  res.status(400).json({success : false, message: `No person found with id ${id}`})
+       }
+       const newPeople = people.map((person)=>{
+           if(person.id === Number(id)){
+            person.name = name;
+           }
+           return person;
+       })
+       res.status(200).json({success: true, data :newPeople})
+  })
+
+// delete method
+
+app.delete('/api/people/postman/:id', (req, res)=>{
+    const {id} = req.params;
+    const {name} = req.body;
+    const person = people.find((person)=>person.id === Number(id))
+    if(!person){
+      return  res.status(401).json({success : false , message : `not found id ${id}`})
+    }
+    const newData = people.filter((person)=>person.id !== Number(id))
+    res.status(200).json({success : true, data : newData});
+})
+
 app.listen(5000, ()=>{
     console.log("Server is listing on port 5000");
 })
